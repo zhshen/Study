87c83b75b56da56f52b752810617a7ac18766758
Zheng Shen <shenzheng72@hotmail.com>
2015-11-01 11:29:47 +0800

    调整新工程

--- AngularJS/PhoneCat/Properties/AssemblyInfo.cs
+++ AngularJS/PhoneCat/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1ba5ee2f-ee32-4928-8087-1aa7c374b36c")]
+[assembly: Guid("804fefaa-4017-4d65-91e0-51adcb49d430")]
 
 // Version information for an assembly consists of the following four values:
 //
