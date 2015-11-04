199bdaf9ce6ca131d1dc9b8c3aef6c66e2cae186
Zheng Shen <shenzheng72@hotmail.com>
2015-11-04 15:49:45 +0800

    调整格式

--- AngularJS/PhoneCat/demos/app.js
+++ AngularJS/PhoneCat/demos/app.js
@@ -1,11 +1,20 @@
-﻿var phonecatApp = angular.module('phonecatApp', [  'ngRoute',  'phonecatControllers']);
+﻿var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatControllers'
+]);
 
-phonecatApp.config(['$routeProvider',  function ($routeProvider) {
-      $routeProvider.        when('/phones', {
-            templateUrl: 'partials/phone-list.html',            controller: 'PhoneListCtrl'
-        }).        when('/phones/:phoneId', {
-            templateUrl: 'partials/phone-detail.html',            controller: 'PhoneDetailCtrl'
-        }).        otherwise({
+phonecatApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/phones', {
+            templateUrl: 'partials/phone-list.html',
+            controller: 'PhoneListCtrl'
+        }).
+        when('/phones/:phoneId', {
+            templateUrl: 'partials/phone-detail.html',
+            controller: 'PhoneDetailCtrl'
+        }).
+        otherwise({
             redirectTo: '/phones'
         });
   }]);
